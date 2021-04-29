3c900df3026d9e3aaac408616c6c9be26a030c5a
Carlos Anoceto <canoceto@corp.optaresolutions.com>
2021-04-29 12:17:32 +0200

    First commit

---
+++ src/main/java/com/clearpay/demo/ClearpayDashboardApplication.java
@@ -0,0 +1,13 @@
+package com.clearpay.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClearpayDashboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClearpayDashboardApplication.class, args);
+	}
+
+}
